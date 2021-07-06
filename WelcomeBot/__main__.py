743442870086a8a50c1e2522892f40efd1db5312
XemonDev <84271113+XemonDev@users.noreply.github.com>
2021-07-06 17:30:45 +0430

    Update __main__.py

--- WelcomeBot/__main__.py
+++ WelcomeBot/__main__.py
@@ -22,7 +22,8 @@ def admin_only(func: Callable) -> Callable:
 def welcome_on(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         if cur.execute("SELECT * FROM welcome").fetchone()['status'] == 'on' and message.chat.id in CHATS:
-            return await func(client, message)
+            if message.from_user.id != message.new_chat_members[0].id:
+                return await func(client, message)
 
     return decorator
 
